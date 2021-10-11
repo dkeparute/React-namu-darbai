a28940d5ef1457d61a906b09b2c704f9b8f630ec
Dovilė <dovile.keparute@gmail.com>
2021-10-11 22:05:27 +0300

    Namų darbai Nr. 1

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import Box from "./Components/Box";
+
+const data = [
+  {backgroundColor: '#A621FF', 
+  borderColor: '#E8C4FF'
+  },
+  {backgroundColor: '#FF4E44', 
+  borderColor: '#C1FFFC'
+  },
+  {backgroundColor: '#E7FF7F', 
+  borderColor: '#2D42FF'
+  },
+  {backgroundColor: '#3AF8FF', 
+  borderColor: '#FF3FDB'
+  },
+  {backgroundColor: '#FF3FDB', 
+  borderColor: '#3AF8FF'
+  },
+  {backgroundColor: '#2D42FF', 
+  borderColor: '#E7FF7F'
+  },
+  {backgroundColor: '#C1FFFC', 
+  borderColor: '#FF4E44'
+  },
+  {backgroundColor: '#E8C4FF', 
+  borderColor: '#A621FF'
+  },
+]
+
+function App() {
+  return (
+    <>
+    {data.map((box, index) =><Box key={index} backgroundColor={box.backgroundColor} borderColor= {box.borderColor} /> )}
+
+    
+      {/* <Box backgroundColor={'#A621FF'} borderColor= {'#E8C4FF'} /> */}
+      {/* <Box backgroundColor={'#FF4E44'} borderColor= {'#C1FFFC'}/>
+      <Box backgroundColor={'#E7FF7F'} borderColor= {'#2D42FF'}/>
+      <Box backgroundColor={'#3AF8FF'} borderColor= {'#FF3FDB'}/>
+      <Box backgroundColor={'#FF3FDB'} borderColor= {'#3AF8FF'}/>
+      <Box backgroundColor={'#2D42FF'} borderColor= {'#E7FF7F'}/>
+      <Box backgroundColor={'#C1FFFC'} borderColor= {'#FF4E44'}/>
+      <Box backgroundColor={'#E8C4FF'} borderColor= {'#A621FF'}/> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Box.jsx
@@ -0,0 +1,12 @@
+
+function Box({backgroundColor, borderColor}) {
+
+
+    return (
+        <div className="black-box" style={{background: backgroundColor, borderColor: borderColor}}>
+         
+        </div>
+    )
+}
+
+export default Box;
