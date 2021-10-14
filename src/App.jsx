b25e15aef9c6c5def44addc1a4caf201cecb9153
Dovilė <dovile.keparute@gmail.com>
2021-10-14 16:33:56 +0300

    Namų darbas: Burbulai

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Bubble from "./Components/Bubble";
+
+function App() {
+
+    return (
+        <div>
+            <Bubble text={'1'} />
+            <Bubble text={'2'} />
+            <Bubble text={'3'} />
+            <Bubble text={' 4'} />
+            <Bubble text={'5'} />
+            <Bubble text={'6'} />
+            <Bubble text={'7'} />
+            <Bubble text={'8'} />
+            <Bubble text={'9'} />
+            <Bubble text={'10'} />
+        </div>
+    );
+}
+export default App;
