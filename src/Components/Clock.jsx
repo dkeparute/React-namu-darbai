af94e776422318b91bfce6340e1e3b34188fd749
Dovilė <dovile.keparute@gmail.com>
2021-10-14 14:37:31 +0300

    Clock

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,3 +1,40 @@
+// import { Component } from "react";
+
+// class Clock extends Component {
+
+//     constructor(props) {
+//         super();
+//         this.state = {
+//             clock: new Date().toLocaleTimeString(),
+//             color: '#' + Math.floor(Math.random()*16777215).toString(16)
+//         };
+//     }
+
+//     componentDidMount() {
+
+//         setInterval(
+//             () => this.tick(),
+//             1000
+//         );
+//     }
+//     // BUSENOS KEITIMAS
+//     tick() {
+//         this.setState({
+//             clock: new Date().toLocaleTimeString(),
+//             color: '#' + Math.floor(Math.random()*16777215).toString(16)
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <h1 style={{backgroundColor: this.state.color}}>{this.state.clock}</h1>
+//         );
+//     }
+
+// }
+// export default Clock;
+
+
 import { Component } from "react";
 
 class Clock extends Component {
@@ -6,28 +43,44 @@ class Clock extends Component {
         super();
         this.state = {
             clock: new Date().toLocaleTimeString(),
-            color: '#' + Math.floor(Math.random()*16777215).toString(16)
+            color: '#' + Math.floor(Math.random() * 16777215).toString(16)
         };
     }
 
     componentDidMount() {
 
-        setInterval(
-            () => this.tick(),
-            1000
-        );
+        // setInterval(
+        //     () => this.tick(),
+        //     1000
+        // );
     }
-    // BUSENOS KEITIMAS
+
     tick() {
         this.setState({
             clock: new Date().toLocaleTimeString(),
-            color: '#' + Math.floor(Math.random()*16777215).toString(16)
+            color: '#' + Math.floor(Math.random() * 16777215).toString(16)
+        });
+    }
+
+    // click()
+
+    click = () => {
+        // console.log(this);
+        this.setState({
+            clock: new Date().toLocaleTimeString(),
+            color: '#' + Math.floor(Math.random() * 16777215).toString(16)
         });
     }
 
     render() {
         return (
-            <h1 style={{backgroundColor: this.state.color}}>{this.state.clock}</h1>
+            <>
+                <h1 style={{ backgroundColor: this.state.color }}>{this.state.clock}
+                    <button onClick={this.click}>CLICK</button>
+                    {/* <button onClick={() => this.click()}>CLICK</button> */}
+                </h1>
+
+            </>
         );
     }
 
