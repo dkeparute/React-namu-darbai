b6c203d065d900d914412ca53a5c6a66d4ac05ae
Dovilė <dovile.keparute@gmail.com>
2021-10-18 21:18:55 +0300

    Nd. 2

---
+++ src/App4.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import Bubble from "./Components/Bubble";
+
+class App extends Component {
+    constructor(props) {
+        super();
+        this.state= {
+            count: 0
+        };
+    }
+
+    score = () => {
+        this.setState ({
+            count: this.state.count +1
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Susprogusiu burbulu suma:{this.state.count}</h1>
+                <Bubble score={this.score} text={'1'}/>
+                <Bubble score={this.score} text={'2'}/>
+                <Bubble score={this.score} text={'3'}/>
+                <Bubble score={this.score} text={'4'}/>
+                <Bubble score={this.score} text={'5'}/>
+                <Bubble score={this.score} text={'6'}/>
+                <Bubble score={this.score} text={'7'}/>
+                <Bubble score={this.score} text={'8'}/>
+                <Bubble score={this.score} text={'9'}/>
+                <Bubble score={this.score} text={'10'}/>
+            </div>
+        );
+    }
+}
+export default App;
