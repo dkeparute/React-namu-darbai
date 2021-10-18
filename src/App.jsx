39f5afd8ea4cf61c35540748cc663a81dbfa0edc
Dovilė <dovile.keparute@gmail.com>
2021-10-18 21:19:03 +0300

    Nd. 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import { Component } from "react";
-import Bubble from "./Components/Bubble";
 
-class App extends Component {
-    constructor(props) {
-        super();
-        this.state= {
-            count: 0
-        };
-    }
+import Circle from "./Components/Circle";
+// PAGRINDINIS KOMPONENTRAS KURIS SURISA KITUS KOMPONENTUS IR PERDUODA VISKA REACTO RENDINIMUI
+function App() {
 
-    score = () => {
-        this.setState ({
-            count: this.state.count +1
-        });
-    };
-
-    render() {
-        return (
-            <div>
-                <h1>Suma:{this.state.count}</h1>
-                <Bubble score={this.score} text={'1'}/>
-                <Bubble score={this.score} text={'2'}/>
-                <Bubble score={this.score} text={'3'}/>
-                <Bubble score={this.score} text={'4'}/>
-                <Bubble score={this.score} text={'5'}/>
-                <Bubble score={this.score} text={'6'}/>
-                <Bubble score={this.score} text={'7'}/>
-                <Bubble score={this.score} text={'8'}/>
-                <Bubble score={this.score} text={'9'}/>
-                <Bubble score={this.score} text={'10'}/>
-            </div>
-        );
-    }
+    return (
+        <>
+            <h2>Nd. 3</h2>
+            <Circle />
+        </>
+    );
 }
 export default App;
---
+++ src/Components/Circle.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function Circle() {
+
+    const [counter, setCounter] = useState(0);
+    const [counterNew, setCounterNew] = useState(0);
+
+    const addNumber = () => {
+        setCounter(counter + 1)
+    }
+
+    const addNewNumber = () => {
+        setCounterNew(counterNew + counter)
+    }
+
+    const reset = () => {
+        setCounter(0);
+        setCounterNew(0);
+    }
+
+    return (
+        <>
+            <div className='circle'>
+                <div className='counter'>{counter}</div>
+                <button className='btn' onClick={() => addNumber()}>Spausti 1</button>
+            </div>
+            <div className='circle'>
+                <div className='counter'>{counterNew}</div>
+                <button onClick={() => addNewNumber()}>Spausti 2</button>
+                <button onClick={() => reset(0)}>Atstatyti</button>
+            </div>
+        </>
+    );
+}
+export default Circle;
