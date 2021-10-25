8bed9e8cc9cf996054d550a9a72493f62416579f
Dovilė <dovile.keparute@gmail.com>
2021-10-25 18:17:43 +0300

    Namų darbas Nr. 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,116 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GroupAnimal from "./Components/GroupAnimal";
+import idGenerator from "./Common/idGenerator";
 
 function App() {
-    // PIRMAS LAUKAS
-    const [fieldFirst, setFieldFirst] = useState([]);
 
-    // ANTRAS LAUKAS
-    const [fieldSecond, setFieldSecond] = useState([]);
+    const [field, setField] = useState([]);
 
-    // LAUKO PASIRINKIMAS
-    const [change, setChange] = useState("first");
+    const [fieldNumber, setFieldNumber] = useState(1);
 
 
-    // SALYGA
     const add = (what) => {
-        // padarome STATE HOOKO KOPIJA kad butu galima keisti busena
-        const fieldFirstCopy = fieldFirst.slice();
-        const fieldSecondCopy = fieldSecond.slice();
 
-        // jeigu onChange metu state bus first reikia prideti gyvuna i first optiona
-        if (change === "first") {
-            fieldFirstCopy.push({ animal: what });
-            // atiduodam kopija
-            setFieldFirst(fieldFirstCopy);
+        const fieldCopy = field.slice();
+
+        fieldCopy.push({
+            id: idGenerator(),
+            animal: what,
+            weight: 0,
+            field: parseInt(fieldNumber)
+        });
+
+        setField(fieldCopy);
+        // NORINT JOG NEISSITRINTU INFORMACIJA
+        localStorage.setItem('animals', JSON.stringify(fieldCopy));
+        console.log(fieldCopy);
+    }
+
+    const selectField = event => {
+        setFieldNumber(event.target.value);
+    }
+    // NORINT JOG PASPAUDUS ISSITRINTU GYVULYS
+    const goHome = (id) => {
+        const fieldCopy = field.slice();
+        const ind = fieldCopy.findIndex(e => e.id === id);
+        fieldCopy.splice(ind, 1);
+        setField(fieldCopy);
+        localStorage.setItem('animals', JSON.stringify(fieldCopy));
+    }
+    // NORINT JOG PASPAUDUS ISVALYTU VISA GRUPE GYVULIU
+    const groupGoHome = (group) => {
+        const fieldCopy = field.slice();
+        while (true) {
+            const ind = fieldCopy.findIndex(e => e.animal === group);
+            if (ind < 0) {
+                break;
+            }
+            fieldCopy.splice(ind, 1);
         }
-        // jeigu onChange metu state bus second reikia prideti gyvuna i second optiona
-        else if (change === "second") {
-            fieldSecondCopy.push({ animal: what });
-            // atiduodam kopija
-            setFieldSecond(fieldSecondCopy);
+        setField(fieldCopy);
+        localStorage.setItem('animals', JSON.stringify(fieldCopy));
+    }
+
+    // APP komponente reikalingas sniuriukas - svorio pridejimas
+
+    const addWeight = (id, w) => {
+        // susikuriam karviu kopija
+        const fieldCopy = field.slice();
+        // susirandam indeksa
+        const i = fieldCopy.findIndex(e => e.id === id);
+        // i ta indeksa savybeje weight irasome nauja svori
+        fieldCopy[i].weight = parseFloat(w);
+        setField(fieldCopy);
+        localStorage.setItem('animals', JSON.stringify(fieldCopy));
+    }
+
+
+
+    useEffect(() => {
+        //  klausiame ar tu turi kokiu nors gyvuliu? Atsakymas bus null - nieko nera
+        const animalsfromStorage = localStorage.getItem('animals');
+        // jeigu yra gyvuliu, tada rasome:
+        if (null !== animalsfromStorage) {
+            // ir pasetinam i STATE
+            setField(JSON.parse(animalsfromStorage));
         }
-    };
-    // select funkcija
-    const changeToWhich = event => {
-        setChange(event.target.value);
-    };
+    }, []);
+
 
     return (
-        <div className="field">
-            <div>
-                {/* sukuriamas komponentas GroupAnimal */}
-                {/* renderinamas pirmas laukas */}
-                {fieldFirst.map((fieldAnimal, i) => (
-                    <GroupAnimal key={i} fieldAnimal={fieldAnimal}></GroupAnimal>
-                ))}
+        <>
+            <div className='field'>
+                <div className='field_part'>
+
+                    {field.map((fieldAnimal, i) => <GroupAnimal key={i} field={1} fieldAnimal={fieldAnimal} goHome={goHome} addWeight={addWeight}></GroupAnimal>)}
+                </div>
+                <div className='field_part'>
+
+                    {field.map((fieldAnimal, i) => <GroupAnimal key={i} field={2} fieldAnimal={fieldAnimal} goHome={goHome} addWeight={addWeight}></GroupAnimal>)}
+                </div>
+                <div className='field_part'>
+
+                    {field.map((fieldAnimal, i) => <GroupAnimal key={i} field={3} fieldAnimal={fieldAnimal} goHome={goHome} addWeight={addWeight}></GroupAnimal>)}
+                </div>
             </div>
-            <div>
-                {/* sukuriamas komponentas GroupAnimal */}
-                {/* renderinamas antras laukas */}
-                {fieldSecond.map((fieldAnimal, i) => (
-                    <GroupAnimal key={i} fieldAnimal={fieldAnimal}></GroupAnimal>
-                ))}
-            </div>
-            {/* mygtuko paspaudimo metu pridedam tam tikra gyvuna */}
-            <button onClick={() => add("cow")}>Add cow</button>
-            <button onClick={() => add("sheep")}>Add sheep</button>
-            <button onClick={() => add("horse")}>Add horse</button>
-            {/* selecto keitimo metu islukstenama funkcija su eventu, priklausomai nuo pasirinkimo bus pateikta reiksme */}
-            <select onChange={event => changeToWhich(event)} value={fieldFirst}>
-                <option value={"first"}>First field</option>
-                <option value={"second"}>Second field</option>
+            <select value={fieldNumber} onChange={selectField}>
+                <option value={1}>Field One</option>
+                <option value={2}>Field Two</option>
+                <option value={3}>Field Three</option>
             </select>
-        </div>
+            <div>
+                <div className='buttons-holder'>
+                    <button onClick={() => add('cow')}>Add cow</button>
+                    <button onClick={() => add('sheep')}>Add sheep</button>
+                    <button onClick={() => add('horse')}>Add horse</button>
+                </div>
+            </div>
+            <div className='buttons-holder'>
+                <button onClick={() => groupGoHome('cow')}>Home cow</button>
+                <button onClick={() => groupGoHome('sheep')}>Home sheep</button>
+                <button onClick={() => groupGoHome('horse')}>Home horse</button>
+            </div>
+        </>
     );
 }
-
 export default App;
--- src/Components/GroupAnimal.jsx
+++ src/Components/GroupAnimal.jsx
@@ -1,21 +1,87 @@
-function GroupAnimal({ fieldAnimal }) {
+// function GroupAnimal({ fieldAnimal }) {
+//     // cia bus CONDITIONAL RENDERINGAS
+
+import { useState } from "react";
+
+//     if ('cow' === fieldAnimal.animal) {
+//         return (
+//             <div className='cow'></div>
+
+//         );
+//     }
+//     else if ('sheep' === fieldAnimal.animal) {
+//         return (
+//             <div className='sheep'></div>
+
+//         );
+//     }
+//     else if ('horse' === fieldAnimal.animal) {
+//         return (
+//             <div className='horse'></div>
+
+//         );
+//     }
+//     else {
+//         // priesingu atveju nieko nerendinam, grazinam null
+//         return null;
+//     }
+
+
+// }
+// export default GroupAnimal;
+
+function GroupAnimal({ fieldAnimal, field, goHome, addWeight }) {
     // cia bus CONDITIONAL RENDERINGAS
 
-    if ('cow' === fieldAnimal.animal) {
+    // UZDEDAMA KONTROLE
+
+    const [weight, setWeight] = useState('');
+
+    // priskiriamas svoris onChange
+    const handleWeight = event => {
+        setWeight(event.target.value);
+    }
+    // pridedamas svoris onClick
+    const submitWeight = () => {
+        addWeight(fieldAnimal.id, weight);
+        // tam kad po setinimo butu tuscia
+        setWeight('');
+
+    }
+
+    if (field === fieldAnimal.field && 'cow' === fieldAnimal.animal) {
         return (
-            <div className='cow'></div>
+            <div className='cow'>
+                <h2>Gyvulio ID: {fieldAnimal.id}</h2>
+                <h2>Gyvulio svoris: {fieldAnimal.weight}</h2>
+                <input type="text" onChange={handleWeight} value={weight} />
+                <button className='btn' onClick={submitWeight}>Add</button>
+                <button className='btn' onClick={() => goHome(fieldAnimal.id)}>Home</button>
+            </div>
 
         );
     }
-    else if ('sheep' === fieldAnimal.animal) {
+    else if (field === fieldAnimal.field && 'sheep' === fieldAnimal.animal) {
         return (
-            <div className='sheep'></div>
+            <div className='sheep'>
+                <h2>Gyvulio ID:{fieldAnimal.id}</h2>
+                <h2>Gyvulio svoris: {fieldAnimal.weight}</h2>
+                <input type="text" onChange={handleWeight} value={weight} />
+                <button className='btn' onClick={submitWeight}>Add</button>
+                <button className='btn' onClick={() => goHome(fieldAnimal.id)}>Home</button>
+            </div>
 
         );
     }
-    else if ('horse' === fieldAnimal.animal) {
+    else if (field === fieldAnimal.field && 'horse' === fieldAnimal.animal) {
         return (
-            <div className='horse'></div>
+            <div className='horse'>
+                <h2>Gyvulio ID:{fieldAnimal.id}</h2>
+                <h2>Gyvulio svoris: {fieldAnimal.weight}</h2>
+                <input type="text" onChange={handleWeight} value={weight} />
+                <button className='btn' onClick={submitWeight}>Add</button>
+                <button className='btn' onClick={() => goHome(fieldAnimal.id)}>Home</button>
+            </div>
 
         );
     }
