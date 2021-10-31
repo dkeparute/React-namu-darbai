579df8a474d99d67a18dcc874d09233199c0cb45
Dovilė <dovile.keparute@gmail.com>
2021-10-31 20:52:45 +0200

    ok

--- src/Components/VienasTurtas.jsx
+++
@@ -1,9 +0,0 @@
-function VienasTurtas({ animal }) {
-// 6. tam komponentui priskiriamas propsas
-    return (
-        <div className='vienas-turtas'>
-            <div>{animal.product}</div>
-        </div>
-    );
-}
-export default VienasTurtas;
