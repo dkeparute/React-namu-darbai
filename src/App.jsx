57f90c1a004d956ab23a0230066487efa27057e7
Dovilė <dovile.keparute@gmail.com>
2021-11-06 16:45:00 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import CreateJewelry from "./Components/CreateJewelry";
-// import JewelryList from "./Components/JewelryList";
-// function App() {
+function App() {
 
-//     const [allJewelries, setAllJewelries] = useState([]);
 
-//     useEffect(() => {
-//         axios.get('http://localhost:3003/jewelry')
-//             .then(res => {
-//                 setAllJewelries(res.data);
-//                 console.log(res.data);
-//             })
-//     }, [])
-
-//     return (
-//         <div className='turtai'>
-//             <CreateJewelry />
-//             <JewelryList allJewelries={allJewelries} />
-//         </div>
-//     );
-// }
-// export default App;
+    return(
+        <div></div>
+    );
+}
+export default App;
